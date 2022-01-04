1366d3d548c90757bcb5c913c0058902902ca11c
Reza <reza.mansouri70@gmail.com>
2022-01-04 11:09:22 +0330

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SampleGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
